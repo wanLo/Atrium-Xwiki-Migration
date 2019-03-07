32dea1e175efece985372efc8955a5455c0f745a
Georg Ahnert <mail@geahnert.de>
2019-03-07 12:18:35 +0100

    Refactor

--- md_format_tables.py
+++ md_format_tables.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python
 
-# files to be replaced with REST-Requests
-file1 = open('table.txt')
-md_input = file1.read()
-file2 = open('table2.txt','w')
-
-#iterate over lines
-lines = md_input.split('\n')
-md_output = ''
-
-i=0
-
-while (i<len(lines)):
+def md_convert_tables(md_input):
 	
-	#append all following lines of a table row
-	if (lines[i][:1]=='|'):
-		while(i<len(lines) and lines[i][-1:]!='|'):
-			md_output += lines[i] + ' '
-			i+=1
-		#last line ends with |
-		md_output += lines[i]
+	#iterate over lines
+	lines = md_input.split('\n')
+	md_output = ''
 
-	#otherwise this is not a table
-	else:
-		md_output += lines[i]
-	
-	md_output += '\n'
-	i+=1
+	i=0
 
-file2.write(md_output)
+	while (i<len(lines)):
+		
+		#tables start with |
+		if (lines[i][:1]=='|'):
+
+			# append all lines belonging to the same table row
+			while(i<len(lines) and lines[i][-1:]!='|'):
+				md_output += lines[i] + ' '
+				i+=1
+			#last line ends with |
+			md_output += lines[i]
+
+		#otherwise this is not a table
+		else:
+			md_output += lines[i]
+		
+		md_output += '\n'
+		i+=1
+
+	return md_output
+
+
+def md_files_convert():
+	input_file  = open('table.txt')
+	output_file = open('table2.txt','w')
+	output_file.write(md_convert_tables(input_file.read()))
+
+
+md_files_convert()
